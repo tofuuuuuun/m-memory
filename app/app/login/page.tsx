2e932b1d3d081cce0083a9a1b19c27d95aec398f
hirano <hirakenlp21@gmail.com>
2025-09-09 22:12:08 +0900

    ログインページのレイアウトを更新し、ヘッダーとフッターを追加、アカウント連携ボタンを実装

--- app/app/login/page.tsx
+++ app/app/login/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import Footer from "@/app/components/common/footer";
+import Header from "@/app/components/layout/header";
+
 export default function Login() {
     return (
-        <div>
-            <h1>Login Page</h1>
+        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+            <Header />
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+                <div>
+                    <h1 className="text-4xl sm:text-5xl font-bold text-center sm:text-left">アカウント連携</h1>
+                    <div>
+                        <button className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]" onClick={() => (window.location.href = "/api/auth/login")}>
+                            連携する
+                        </button>
+                    </div>
+                </div>
+            </main>
+            <Footer />
         </div>
     );
 }
